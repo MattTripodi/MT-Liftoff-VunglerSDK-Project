8e981a829e7f6f5f6ee46d774255a223688f9e9a
MattTripodi <mtripodi1027@gmail.com>
2024-01-25 15:40:42 -0500

    Initial Commit

---
+++ MT-Liftoff-VunglerSDK-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MT-Liftoff-VunglerSDK-Project
+//
+//  Created by Matthew Tripodi on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
